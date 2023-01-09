beb8749f57f0b9e504d2c4eb9b578b8ada794eb0
MartinGarcia26 <martingarciadottori@gmail.com>
2023-01-09 15:40:06 -0300

    Modificando el proyecto

--- index.js
+++ index.js
@@ -12,12 +12,13 @@ do {
 cuotas = parseInt(prompt('Ingrese las cuotas 3, 6 o 12'))
 if (cuotas === 3 || cuotas === 6 || cuotas === 12) {
     precioEnCuotas = (total / cuotas).toFixed(2);
+    console.log(parseInt(precioEnCuotas));
+    alert(`Debe pagar ` + precioEnCuotas + ` en ` + cuotas + ` cuotas`)
 } else {
     alert(`Solo podes elegir entre 3, 6 y 12 cuotas.`)
 }
 
-console.log(parseInt(precioEnCuotas));
-alert(`Debe pagar ` + precioEnCuotas + ` en ` + cuotas + ` cuotas`)
+
 
 
 
